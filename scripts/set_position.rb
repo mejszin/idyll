ff8ba308c5b8d13ec7ba751e377bb6bfff26602e
Louis Machin <ldhmachin@gmail.com>
2022-11-07 15:33:27 +0000

    Added debug logging in API

--- scripts/set_position.rb
+++ scripts/set_position.rb
@@ -1,3 +1,3 @@
 require_relative './api.rb'
 
-puts set_position('test_world_a', 1, 1, '9rDaFwqi')
+puts set_position('test_world_a', 1, 1, '')
--- server/api/lib/index.js
+++ server/api/lib/index.js
@@ -124,6 +124,7 @@ methods.findCredentials = async (username, password) => {
     return new Promise((resolve, reject) => {
         Object.keys(user_data).forEach(async function(token) {
             if (username == user_data[token].username) {
+                console.log('Comparing credentials for', username, 'password=', password, 'hash=', user_data[token].password.hash);
                 const result = await bcrypt.compare(password, user_data[token].password.hash);
                 resolve(result ? token : null);
             } else {
